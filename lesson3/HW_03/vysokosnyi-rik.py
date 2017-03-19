23dc49c18a83291a5e83ca536f336f918884a124
BShept <bshept@ukr.net>
2017-03-19 22:25:26 +0200

    Sprostyv perevirku umovy

--- lesson3/HW_03/vysokosnyi-rik.py
+++ lesson3/HW_03/vysokosnyi-rik.py
@@ -2,8 +2,6 @@ from math import *
 
 k = input ("Vvedy rik k = ")
 
-if k%4 == 0:
-    if k%100 != 0 or (k%100 == 0 and k%400 == 0):
+if k%4 == 0 and k%100 != 0 or k%400 == 0:
         print "Vysokosnyi rik"
-    else: print  "Ne vysokosnyi rik"
 else: print  "Ne vysokosnyi rik"
