2acdd990954d416712feaa920657b09ddc501186
BShept <bshept@ukr.net>
2017-03-19 20:43:57 +0200

    Dodav zavdanny pro rik misyak den

---
+++ lesson3/HW_03/rik-misyak-den.py
@@ -0,0 +1,9 @@
+from math import *
+
+a = input ("Vvedy rik = ")
+b = input ("Vvedy misyak = ")
+c = input ("vvedy den = ")
+
+if a > 0 and 1 <= b <=12 and 1 <= c <= 31:
+    print "Korektno"
+else: print "Ne korektno"
