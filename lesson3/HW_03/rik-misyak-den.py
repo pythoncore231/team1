38955e2af9bd0f2bdc9272b94573a48f8e131c88
BShept <bshept@ukr.net>
2017-03-19 21:48:02 +0200

    Fix vysokosnyi rik i ne parni misyaci

--- lesson3/HW_03/rik-misyak-den.py
+++ lesson3/HW_03/rik-misyak-den.py
@@ -4,6 +4,18 @@ a = input ("Vvedy rik = ")
 b = input ("Vvedy misyak = ")
 c = input ("vvedy den = ")
 
-if a > 0 and 1 <= b <=12 and 1 <= c <= 31:
+if 1 <= c <= 31 and (b%2)!=0 and  1 <= b <=12:
     print "Korektno"
+    print "{}.{}.{}".format(a, b, c)
+elif 1 <= c <= 28 and b == 2:
+    print "Korektno"
+    print "{}.{}.{}".format(a, b, c)
+elif a % 4 == 0 and 1 <= c <= 29 and b == 2:
+    if a % 100 != 0 or (a % 100 == 0 and a % 400 == 0):
+        print "Korektno"
+        print "Vysokosnyi rik"
+        print "{}.{}.{}".format(a, b, c)
+    else:
+        print  "Ne korektno rik Ne vysokosnyi"
+
 else: print "Ne korektno"
