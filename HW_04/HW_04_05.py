2833a758ca3304ee1c5217024e885ce3c26d6b00
vova1995 <vovapetryna@1995gmail.com>
2017-03-23 01:30:46 +0200

    changes

---
+++ HW_04/HW_04_05.py
@@ -0,0 +1,8 @@
+"""The set text display all words beginning with a sequence
+symbols."""
+text = " gdsgdgsergdsgs dsgdsgewgsd sdgsdgdsv "
+t = " "
+for i in text:
+    if 'g' in text and 'd' in text and 's' in text:
+        if int(text[t + 1]) == 'g' and 'd' and 's':
+            print text
