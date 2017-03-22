b5514c207f8e9c844d7200987bde1425921151bf
Anastasiia Navozhko <anastasiia.navozhko@gmail.com>
2017-03-22 23:28:53 +0200

    hw 4

--- hw_4/6_function_calculation.py
+++ hw_4/6_function_calculation.py
@@ -1,8 +1,8 @@
 from math import *
 
-def doublefactorial(x):
+def doublefact(x):
     if x > 0:
-        return doublefactorial(x - 2) * x
+        return doublefact(x - 2) * x
     else:
         return 1
 
@@ -10,7 +10,8 @@ e = 1E-5  # 10**-5
 a = 0.05
 b = 0.9
 h = 0.1
-fS = ('k*(k+1)*x**k', '(1./(factorial(k)*(k+2)))*x**(k+2)', 'k*(k+2)*x**k', 'doublefactorial(2*k+3)/doublefactorial(2*k)*(-x)**k', '(2*k+1)*x**k')
+fS = ('k*(k+1)*x**k', '(1./(factorial(k)*(k+2)))*x**(k+2)', 'k*(k+2)*x**k', \
+      'doublefact(2*k+3)/doublefact(2*k)*(-x)**k', '(2*k+1)*x**k')
 fy = ('2*x/((1-x)**3)', '(x-1)*e**x+1', 'x*(3-x)/(1-x)**3', '(1+x)**(5./2)', '(1+x)/(1-x)**2')
 tk = (1, 0, 1, 0, 0)
 template = "|{:11.5f}\t|{:11.5f}\t|{:11.5f}\t|{:11.5f}\t|"
