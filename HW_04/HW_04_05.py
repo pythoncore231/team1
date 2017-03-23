c8c49994fecb959855fb68c7133a6965df76ea09
vova1995 <vovapetryna@1995gmail.com>
2017-03-23 16:06:51 +0200

    one more changes

--- HW_04/HW_04_05.py
+++ HW_04/HW_04_05.py
@@ -1,8 +1,7 @@
 """The set text display all words beginning with a sequence
 symbols."""
-text = " gdsgdgsergdsgs dsgdsgewgsd sdgsdgdsv "
-t = " "
-for i in text:
-    if 'g' in text and 'd' in text and 's' in text:
-        if int(text[t + 1]) == 'g' and 'd' and 's':
-            print text
+text = "Hello world" * 5
+seq = text.find("Hell")
+while seq == True:
+	print seq
+	break
