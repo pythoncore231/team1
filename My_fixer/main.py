9604e855d4e91b75346a2f603631397b99087294
vodachuk <vodachuk@gmail.com>
2017-04-06 17:28:57 +0300

    modified main.py and print_data.py

--- My_fixer/main.py
+++ My_fixer/main.py
@@ -3,7 +3,7 @@ Created on Apr 5, 2017
 
 @author: vodachuk
 '''
-from utils.print_data import difference, exchange, Condition, print_dict_rates
+from utils.print_data import difference, exchange, Condition, print_dict_rates, yesterday, get_rates_by_period_from_to
 from fixer.data_manager import get_rates
 
 if __name__ == '__main__':
@@ -14,4 +14,8 @@ if __name__ == '__main__':
     data = get_rates("2017-01-25")
     print "\n", "Rate for the TRY on 2017-01-25"
     Condition(data, "TRY")
+#     date = get_rates(base = "USD")["date"]
+
+    data = [i for i in get_rates_by_period_from_to("2013-01-01", "2013-01-25", 'IDR', 'CZK')]
+    print "min = {}".format(min(data))
     
--- My_fixer/utils/print_data.py
+++ My_fixer/utils/print_data.py
@@ -4,8 +4,9 @@ Created on Apr 3, 2017
 @author: vodachuk
 '''
 from fixer.data_manager import get_rates
+import time
+import datetime
 from datetime import date
-# import datetime
 
 def date_validations(date):
     """
@@ -87,6 +88,44 @@ def print_dict_rates(obj):
 
     pass
 
+def print_dict_rates_by_period(data):
+    """
+    :param data(generator):
+    :return None:
+    """
+    # for i in data:
+    #     print_dict_rates(i)
+
+    l = []
+    for obj in data:
+        l.append(obj.get('rates'))
+    rates = zip(l[0], l[0].values(), l[1].values(), l[2].values())
+    for i in rates:
+        for j in i:
+            print "{}\t".format(j),
+        print
+        
+def get_rates_by_period_from_to(start_date, end_date, base_from, base_to):
+    """
+    :param start_date(str):
+    :param end_date(str):
+    :param base(str):
+    :return generator:
+    """
+
+    start_date = map(int, start_date.split("-"))
+    start_date = datetime.date(*start_date)
+
+    end_date = map(int, end_date.split("-"))
+    end_date = datetime.date(*end_date)
+
+    while start_date <= end_date:
+        date = str(start_date)
+        data = get_rates(date, base_from)
+        rate = data.get('rates').get(base_to)
+        yield rate
+        start_date = start_date + datetime.timedelta(days=1)
+
 def exchange(amount, rates, to):
     """
     :parameter amount(int):
@@ -101,7 +140,8 @@ def exchange(amount, rates, to):
             for every in rates_dict[each]:
                 if every == to:
                     result = amount * rates_dict[each][every]
-    return u"{} {} is {} {}".format(amount, rates, result, to)
+    return result
+#     return u"{} {} is {} {}".format(amount, rates, result, to)
 
 def dict_rec(obj):
     for each in obj:
@@ -163,7 +203,47 @@ def Condition(obj, currency = None):
 # def Get_val(data, tup1 = date(date.year, date.month, date.day), tup2 = date(date.year, date.month, date.day), currency):
 #     today = date.today()
 #     
-    
+def yesterday(last_date):
+    day = int(last_date[8:10])
+    month = int(last_date[5:7])
+    year = int(last_date[:4])
+    leap = year % 400 == 0 or year % 4 == 0 and not (year % 100) == 0
+
+    yes_ter_day = day - 1
+
+    if yes_ter_day < 1:
+        month -= 1
+        if month < 1:
+            month += 12
+            year -= 1
+        if month in (1, 3, 5, 7, 8, 10, 12):
+            yes_ter_day += 31
+        if month in (4, 6, 9, 11):
+            yes_ter_day += 30
+        if month == 2 and leap:
+            yes_ter_day += 29
+        if month == 2 and not leap:
+            yes_ter_day += 28
+
+        if len(str(month)) < 2:
+            yes_ter_day_month = "0{}".format(month)
+        else:
+            yes_ter_day_month = "{}".format(month)
+        if len(str(yes_ter_day)) < 2:
+            yes_ter_day_day = "0{}".format(yes_ter_day)
+        else:
+            yes_ter_day_day = "{}".format(yes_ter_day)
+        return "{}-{}-{}".format(year, yes_ter_day_month, yes_ter_day_day)
+    else:
+        if len(str(month)) < 2:
+            yes_ter_day_month = "0{}".format(month)
+        else:
+            yes_ter_day_month = "{}".format(month)
+        if len(str(yes_ter_day)) < 2:
+            yes_ter_day_day = "0{}".format(yes_ter_day)
+        else:
+            yes_ter_day_day = "{}".format(yes_ter_day)
+        return "{}-{}-{}".format(year, yes_ter_day_month, yes_ter_day_day)
     
     
      
