fc4f7cebc449616e86344643ae5b572b1642537d
Volodymyr Sheptitsky <vsheptc@training.local>
2017-04-13 21:04:05 +0300

    Explanation of -str- and -repr- methods.

--- lesson11/uni/entity/room.py
+++ lesson11/uni/entity/room.py
@@ -9,14 +9,23 @@ class Room(Base):
         self.capacity = capacity
     def __str__(self):
         return "id:{} name:{} capacity:{}".format(self.id, self.name, self.capacity)
-    def __repr__(self):
-        return "{}".format(self.name)
+    # def __repr__(self):
+    #     return "{}".format(self.name)
     def to_unicode(self):
         return unicode("{} {} {}".format(self.id, self.name, self.capacity))
 
 
- super(Room,self).__init__(id)
+ # super(Room,self).__init__(id)
 
 # self.__class__ = Base
 # self.__init__()
 # self.__class__ = Room
+
+# class List():
+#   l = (1,2,3,4)
+#   def __str__(self):
+#        template = "["
+#        for i in l:
+#            template += i.__repr__()
+#        template += "]"
+#        return template
